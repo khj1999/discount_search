45623d5a8061d6e1ee7ba8e88c0cbccb32fe6390
khj1999 <mo481872@gmail.com>
2023-03-08 00:05:21 +0900

    Generate project
    프로젝트 생성

---
+++ discount_search/src/main/kotlin/com/example/discount_search/DiscountSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.example.discount_search
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscountSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscountSearchApplication>(*args)
+}
---
+++ discount_search/src/test/kotlin/com/example/discount_search/DiscountSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.discount_search
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscountSearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
